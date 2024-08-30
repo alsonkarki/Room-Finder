3a3403b959e8a05e42418bdd34138b50842efabf
Alson07 <karkialson321@gmail.com>
2024-08-30 17:27:03 +0545

    migration

---
+++ Migrations/20240830094620_changeson.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomFInder.Migrations
+{
+    public partial class changeson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
